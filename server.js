ae7d28fb75f4fec218b65ef67a3ffeb0610022e1
Victoria Dunlap <victoriacdunlap@gmail.com>
2022-07-19 07:31:49 -0400

    starting framework set up

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const data = require('./db/data.json')
+const path = require('path');
+
+const PORT = 5511;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  });
+
+app.post('/api/reviews', (req, res) => {
+    res.json(`${req.method} request
+    received`)
+})
+
+app.delete('/', (req, res) => {
+     res.send('DELETE request to homepage')
+})
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
+})
